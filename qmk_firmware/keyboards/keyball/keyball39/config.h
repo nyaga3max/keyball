0f2de861a604f9ca7fd714c4f4f899b732d0da42
nyaga3max <armageddon_clock@hotmail.co.jp>
2025-12-18 06:59:26 +0900

    Enable dynamic keymap layer count

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -78,4 +78,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 
-//#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
